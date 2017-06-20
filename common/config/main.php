6b928973194629636c31ef6d3128c4aab029bd58
Lucas Dias <diasnlucas@gmail.com>
2017-06-20 12:02:10 -0300

    Update main.php

--- common/config/main.php
+++ common/config/main.php
@@ -9,7 +9,7 @@ return [
 	    'class' => 'yii\db\Connection',
 	    'dsn' => 'mysql:host=localhost;dbname=yiiplus',
 	    'username' => 'root',
-	    'password' => 'tligwww15',
+	    'password' => '',
 	    'charset' => 'utf8',
 	],
     ],
